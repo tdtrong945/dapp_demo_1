96c7d6db184efe0b406992b36e0b5b4b4db4e597
tdtrong945 <dthoccode@gmail.com>
2026-04-03 13:34:53 +0700

    Fix import path for api.js

--- src/components/Member/index.jsx
+++ src/components/Member/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { api } from '../api'
+import { api } from '../../api'
 
 export default function MemberDashboard() {
   const navigate = useNavigate()
